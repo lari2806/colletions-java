dc9400ea221ac6d37acd63674f92485328267e02
Larissa <larissa.rfs28@gmai.com>
2024-07-31 17:38:11 -0300

    Desafio prático sobre list.

---
+++ list/src/ListaTarefa.java
@@ -0,0 +1,45 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import list.Tarefa;
+
+public class ListaTarefa {
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa(){
+        this.tarefaList = new ArrayList<>();
+    }
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if(t.getDescriçao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("número total de tarefas: " + listaTarefa.obterNumeroTotalTarefas());
+        
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        System.out.println("número total de tarefas: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+        
+    }
+
+}
---
+++ list/src/list/Tarefa.java
@@ -0,0 +1,15 @@
+package list;
+public class Tarefa {
+    private String descriçao;
+      public Tarefa(String descriçao){
+        this.descriçao = descriçao;
+      }
+    
+    public String getDescriçao(){
+        return descriçao;
+    }
+    @Override
+    public String toString(){
+        return descriçao;
+    }
+}
