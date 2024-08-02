44fc3ad0c17a96c36fc5f6fdd41b40efdaf522f2
Larissa <larissa.rfs28@gmai.com>
2024-08-02 19:55:21 -0300

    Desáfio prático Agenda de contatos.

---
+++ set/AgendaDeContatos/src/AgendaContatos.java
@@ -0,0 +1,48 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero){
+        contatoSet.add( new Contato(nome, numero));
+    }
+    public void exibirContatos(){
+        System.out.println(contatoSet);
+    }
+    public Set<Contato> pesquisarPorNome(String nome){
+        Set<Contato> contatoPorNome = new HashSet<>();
+        for(Contato c : contatoSet){
+            if (c.getNome().startsWith(nome)) {
+                contatoPorNome.add(c);
+            }
+        }
+        return contatoPorNome;
+    }
+    public Contato atualizarNumeroContato(String nome, int novoNumero){
+        Contato contatoAtualizado = null;
+        for(Contato c : contatoSet){
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+        agendaContatos.adicionarContato("Larissa", 985669102);
+        agendaContatos.adicionarContato("Ariely", 12346867);
+        agendaContatos.adicionarContato("Larissa", 979069102);
+
+        System.out.println(agendaContatos.pesquisarPorNome("Ariely"));
+        System.out.println("Você atualizou número deste contato para: "+ agendaContatos.atualizarNumeroContato("Ariely", 323245678));
+        agendaContatos.exibirContatos();
+    }
+}
