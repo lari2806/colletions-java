d1316b4d83486ecfade38bbd5e817c2558959c72
Larissa <larissa.rfs28@gmai.com>
2024-08-10 19:34:48 -0300

    Modificando arquivos do  pacote Banco Digital.

--- bancoDigital/src/ContaPoupanca.java
+++ bancoDigital/src/ContaPoupanca.java
@@ -1,4 +1,9 @@
 public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(Cliente cliente){
+        super(cliente);
+    }
+
     @Override
     public void imprimirExtrato(){
         System.out.println("=== Extrato conta Poupança ===");
